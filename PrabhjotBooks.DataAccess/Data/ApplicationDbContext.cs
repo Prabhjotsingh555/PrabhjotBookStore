d4005ad598ec8bcb3f930a68cf2797ea1f4ba69b
W0828035 <W0828035@myscc.ca>
2023-11-16 17:11:05 -0500

    Create new folder(Covertype)

--- PrabhjotBooks.DataAccess/Data/ApplicationDbContext.cs
+++ PrabhjotBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,6 @@ namespace PrabhjotBookStore.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
-      
+        public DbSet<CoverType> CoverTypes { get; set; }
     }
 }
---
+++ PrabhjotBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,35 @@
+﻿using PrabhjotBooks.DataAccess.Repository.IRepository;
+using PrabhjotBooks.Models;
+using PrabhjotBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrabhjotBooks.DataAccess.Repository
+{
+    class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(CoverType coverType)
+        {
+            // use .NET LINQ to retrieve the first or default category object,
+            // then pass the id as a generic entity  which matches the Category ID
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null) // save  changes if not null
+            {
+                objFromDb.Name = coverType.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
